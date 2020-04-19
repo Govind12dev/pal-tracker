907b247fef53030a76fe312514d4f4fbf3fbe1d9
Govind12dev <govind12btechit@gmail.com>
2020-04-19 20:55:18 +0800

    Response body text changed to hello

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/")
     public String sayHello(){
-        return "Hello";
+        return "hello";
     }
 }

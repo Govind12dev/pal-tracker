1f125e95ed8304ae3ced2b5cb9fe81908cf26315
Govind12dev <govind12btechit@gmail.com>
2020-04-22 02:44:35 +0800

    space

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
     String message;
+
     public WelcomeController(@Value("${welcome.message}")String message){
         this.message=message;
     }
